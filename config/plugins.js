941f97778a3c7304f0f474910d5dc0cf29775dcb
Camille Sylla <camillesylla.web@gmail.com>
2023-07-20 02:28:35 +0200

    feat(product): add image to gcp

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+const gcp_key = require('../gcp-key.json');
+module.exports = {
+    upload: {
+      config: {
+        provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+        providerOptions: {
+            bucketName: 'media-sto',
+            publicFiles: false,
+            uniform: false,
+            serviceAccount: gcp_key, // replace `{}` with your serviceAccount JSON object
+            baseUrl: 'https://storage.googleapis.com/media-sto',
+            basePath: '',
+        },
+      },
+    },
+}
--- src/api/product/content-types/product/lifecycles.js
+++ src/api/product/content-types/product/lifecycles.js
@@ -4,10 +4,12 @@ module.exports = {
   async beforeCreate(event) {
     const { data, where, select, populate } = event.params;
     try {
+      const gallery = data.Gallery.map(item => item.url);
       const stripePrice = Number(data.Price) * 100;
       const product = await stripe.products.create({
         name: data.Name,
         description: data.Exerpt,
+        images: gallery,
       })
       strapi.log.info(`Product created ${product.name}(id:${product.id})`);
       const price = await stripe.prices.create({
@@ -16,7 +18,7 @@ module.exports = {
         currency: "eur",
       })
       strapi.log.info(`Price created id:${price.id} and added to product id:${product.id}`);
-      event.params.data = { ...data, stripe_id: product.id, price_id: price.id };
+      event.params.data = { ...data, stripe_id: product.id, price_id: price.id, Media: gallery };
     } catch(error) {
       strapi.log.error(error);
     }
