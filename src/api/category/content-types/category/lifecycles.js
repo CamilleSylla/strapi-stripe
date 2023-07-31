62e116fc1f0c7aee021f3099df058814407ad07b
Camille <camille.sylla@valiuz.com>
2023-08-01 00:42:15 +0200

    feat(category): handle errors and add key to header

--- src/api/category/content-types/category/lifecycles.js
+++ src/api/category/content-types/category/lifecycles.js
@@ -1,24 +1,48 @@
-const { stripe } = require("../../../../stripe/stripe");
+const fetch = require('node-fetch');
 
 module.exports = {
 
   async afterCreate() {
-   await buildTree()
+    try {
+      await buildTree()
+    } catch (error) {
+      throw error
+    }
   },
   async afterUpdate(){
-    await buildTree()
+    try {
+      await buildTree()
+    } catch (error) {
+      throw error
+    }
   },
   async afterDelete() {
-    await buildTree()
+    try {
+      await buildTree()
+    } catch (error) {
+      throw error
+    }
   },
 };
 
 const buildTree = async () => {
   strapi.log.info("trigger api to build tree");
   try {
-    return await fetch(`${process.env.API_BASE_URL}/categories-tree/webhook/build`, {
+    const res = await fetch(`${process.env.API_BASE_URL}/categories-tree/webhook/build`, {
       method: "POST",
+      headers: {
+        "Authorization": `Simple ${process.env.API_SK}`
+      }
     })
+
+    if (!res.ok){
+      if (res.status === 403){
+        throw new Error('Forbiden')
+      }
+      throw new Error(res)
+    } else {
+      return 
+    }
   } catch (error) {
     strapi.log.error(error);
     throw error;
